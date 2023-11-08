5688950b883ef3f7265d7f3bb9b0828f4e910260
Adrian Tapia <adrian.tapia@leadonemx.com>
2023-11-08 15:37:45 -0600

    Update suitlet to work with ue

--- practicas/l1_hello_world_ue.js
+++ practicas/l1_hello_world_ue.js
@@ -2,7 +2,7 @@
  *@NApiVersion 2.x
  *@NScriptType UserEventScript
  */
-define([], function() {
+define(['N/redirect'], function(redirect) {
 
     //cuando se carga la pagina, puedes hacer movimientos antes de que se cargue el registro
     function beforeLoad(context) {}
@@ -12,15 +12,24 @@ define([], function() {
 
     //cuando se guarda el registro pasa por esta funcion.
     function afterSubmit(context) {
-        log.debug("hello world");
-
         //get the custom entity field value
         const employee = context.newRecord;
-        const employeeCourse = employee.getValue({
-            fieldId: "custentity_l1_employee_course"
-        });
+        // const employeeCourse = employee.getValue({
+        //     fieldId: "custentity_l1_employee_course"
+        // });
 
-        log.debug("employeeCourse", employeeCourse);
+        // log.debug("employeeCourse", employeeCourse);
+
+        //redirect suitlet, in this example from employee view to the suitlet created:
+        redirect.toSuitelet({ //passing values to the suitelet
+            scriptId: 'customscript_l1_sl_update_emp_notes',
+            deploymentId: 'customdeploy_l1_sl_update_emp_notes',
+            parameters: {
+                l1_name: employee.getValue('entityid'),
+                l1_notes: employee.getValue('comments'),
+                l1_empid: employee.id,
+            },
+        });
     }
 
     return {
--- practicas/l1_sl_update_emp_notes.js
+++ practicas/l1_sl_update_emp_notes.js
@@ -2,54 +2,86 @@
  *@NApiVersion 2.1
  *@NScriptType Suitelet
  */
-define(['N/ui/serverWidget'], function(serverWidget) {
-
+define(['N/ui/serverWidget', 'N/record', 'N/redirect'], function(serverWidget, record, redirect) {
     function onRequest(context) {
         let request = context.request;
         let response = context.response;
 
-        //Create the form object
-        const form = serverWidget.createForm({
-            title: "Employee Notes",
-            hideNavBar: false
-        });
+        //GET - obtain data from UE and create the form (comes from employee view)
+        if(request.method === 'GET') {
+            //Get the parameters from the redirect of the user event script (l1_hello_world_ue.js)
+            const nameFromUe = request.parameters.l1_name;
+            const notesFromUe = request.parameters.l1_notes;
+            const empIdFromUe = request.parameters.l1_empid;
 
-        //Fields for the form
-        const nameField = form.addField({
-            id: "custpage_l1_employee_name",  // lowercase and no spaces
-            type: serverWidget.FieldType.TEXT,
-            label: "Name"
-        });
+            //Create the form object
+            const form = serverWidget.createForm({
+                title: "Employee Notes",
+                hideNavBar: false
+            });
 
-        const notesField = form.addField({
-            id: "custpage_l1_employee_notes",
-            type: serverWidget.FieldType.TEXT,
-            label: "Notes"
-        });
+            //Fields for the form
+            const nameField = form.addField({
+                id: "custpage_l1_employee_name",  // lowercase and no spaces
+                type: serverWidget.FieldType.TEXT,
+                label: "Name"
+            });
 
-        const empId = form.addField({
-            id: "custpage_l1_employee_id",
-            type: serverWidget.FieldType.TEXT,
-            label: "ID"
-        });
+            const notesField = form.addField({
+                id: "custpage_l1_employee_notes",
+                type: serverWidget.FieldType.TEXT,
+                label: "Notes"
+            });
 
-        //save button
-        form.addSubmitButton({
-            label: "Save"
-        });
+            const empId = form.addField({
+                id: "custpage_l1_employee_id",
+                type: serverWidget.FieldType.TEXT,
+                label: "ID"
+            });
 
-        //Disabled the name field
-        nameField.updateDisplayType({
-            displayType: serverWidget.FieldDisplayType.INLINE
-        });
+            //continue button
+            form.addSubmitButton('Continue');
 
-        //Hide employee ID
-        empId.updateDisplayType({
-            displayType: serverWidget.FieldDisplayType.HIDDEN
-        });
+            //Set default values with the parameters data from the redirect of the UE.
+            nameField.defaultValue = nameFromUe;
+            notesField.defaultValue = notesFromUe;
+            empId.defaultValue = empIdFromUe;
 
-        //add the fields and button to the form
-        response.writePage(form);
+            //Disabled the name field
+            nameField.updateDisplayType({
+                displayType: serverWidget.FieldDisplayType.INLINE
+            });
+
+            //Hide employee ID
+            empId.updateDisplayType({
+                displayType: serverWidget.FieldDisplayType.HIDDEN
+            });
+
+            //add the fields and button to the form
+            response.writePage(form);
+        } else { 
+            //POST - update the record and redirect to employee view and set the value on the notes field
+
+            //get the parameters from the form (Js no block scope)
+            empId = request.parameters.custpage_l1_employee_id;
+            notes = request.parameters.custpage_l1_employee_notes;
+
+            //load the record
+            const employee = record.load({
+                type: record.Type.EMPLOYEE,
+                id: empId,
+            });
+
+            //set the value on the notes field
+            employee.setValue('comments', notes);
+            employee.save();
+
+            //redirect to the employee view
+            redirect.toRecord({
+                type: record.Type.EMPLOYEE,
+                id: empId,
+            });
+        }
     }
 
     return {
